093e1551796ccd133c0ad37d5c60ef6e36283219
Vinícius Fuchshuber <vinifux@gmail.com>
2024-09-10 20:24:07 -0300

    adicionado código em python que transforma arquivos txt em gráficos 2D

---
+++ ScanAnalysis/scan_analysis.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+fileName = "scan_Digitizer_50x50.txt"
+#abre o arquivo e salva as duas primeiras linhas como strings
+with open(fileName) as file:
+    line1 = file.readline().strip()
+    line2 = file.readline().strip()
+
+#salva os passos em X e Y como variáveis float
+stepX = float(line1.split("Step X = ")[1].strip().replace(',','.'))
+stepY = float(line2.split("Step Y = ")[1].strip().replace(',','.'))
+
+#salva os dados do arquivo em um array, pulando as duas primeiras linhas
+arr = np.loadtxt(fileName, skiprows=2)
+
+#numero de elementos em X e Y
+nX = arr.shape[1]
+nY = arr.shape[0]
+
+#dimensoes da figura
+fig = plt.figure( figsize=(12,8) )
+#plot em forma de matriz
+g = plt.imshow( arr, origin='lower', extent=( 0., stepX*(nX-1), 0., stepY*(nY-1)), cmap="coolwarm" )
+fig.colorbar(g)
+
+#plot em escala logaritmica
+# plt.imshow( arr, origin='lower', extent=( 0., stepX*(nX-1), 0., stepY*(nY-1)), cmap="plasma", norm=LogNorm() )
+
+plt.savefig( "mapa_log.png", bbox_inches='tight' )
+plt.show()
